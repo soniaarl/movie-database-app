9581ce0ec8faef813d4c6e1f2d67e9024e5899ab
Zahra Nouri <zahra.nouri.ic@gmail.com>
2020-04-25 00:30:06 -0700

    hgfh

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -18,7 +18,8 @@ const AppRouter = () => (
 				<Route path={'/'} exact><Home /></Route>
 				<Route path={'/favourites'} exact><Favourites /></Route>
 				<Route path={'/about'}><About /></Route>
-				<Route path={'/moviepage'}><MoviePage /></Route>
+				{/* <Route path={'/moviepage'}><MoviePage /></Route> */}
+				<Route path={'/*'}><PageNotFound /></Route>
 			</Switch>
 			<Main/>
 			<Footer />
