9177c61dd24cfc9dcea0ef86563910f11428edbb
soniaarl <59911419+soniaarl@users.noreply.github.com>
2020-04-29 15:39:38 -0700

    deleted main.js, added max-width

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -7,7 +7,6 @@ import Home from '../components/Home';
 import About from '../components/About';
 import Favourites from '../components/Favourites';
 import MoviePage from '../components/MoviePage';
-import Main from '../components/Main';
 
 const AppRouter = () => (
 	<Router basename={APP_FOLDER_NAME}>
@@ -18,7 +17,6 @@ const AppRouter = () => (
 				<Route path={'/about'}><About /></Route>
 				<Route path='/movie/:movieId'><MoviePage /></Route>
 			</Switch>
-			<Main/>
 			<Footer />
 	</Router>
 );
